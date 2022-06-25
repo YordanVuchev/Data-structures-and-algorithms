d41d172fc732769df8a87336a81eb13c4d85bf6f
Yordan Vuchev <danivu4e@abv.bg>
2022-06-25 13:30:21 +0300

    practice

---
+++ test.js
@@ -0,0 +1,14 @@
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+console.log(isAlphaNumeric("a"));
