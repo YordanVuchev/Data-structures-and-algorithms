2aed0ad849173ee70655dc6dae5ee1b174a63d03
Yordan Vuchev <danivu4e@abv.bg>
2022-07-01 14:41:04 +0300

    fix

--- Recursion/challenge3.js
+++ Recursion/challenge3.js
@@ -1,11 +1,14 @@
 // Write a function called productOfArray which takes in an array of number and returns the product of them all.
 function productOfArray(arr) {
-  if (arr.length === 1) {
+  if (arr.length <= 1) {
     return arr[0];
   }
   return arr[0] * productOfArray(arr.slice(1));
 }
 
-console.log(productOfArray([1, 2, 3])); //6
+// console.log(productOfArray([1, 2, 3])); //6
 
-console.log(productOfArray([1, 2, 3, 10])); //60
+// console.log(productOfArray([1, 2, 3, 10])); //60
+
+// console.log(productOfArray([5, 6, 2, 1])); //60
+console.log(productOfArray([])); //60
