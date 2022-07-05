14e67c1eb456469f1fcfe4fc67a1abe68039a91b
Yordan Vuchev <danivu4e@abv.bg>
2022-07-05 20:46:24 +0300

    Quick sort

--- SortingAlgorithms/QuickSort.js
+++ SortingAlgorithms/QuickSort.js
@@ -1,14 +1,31 @@
-function pivot(arr) {
-  let number = arr[0];
-  let index = 1;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > number) {
-      let temp = arr[1];
-      arr[index] = arr[i];
-      arr[i] = temp;
-      index++;
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
     }
   }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
 }
 
-pivot([5, 8, 10, 2, 6, 1]);
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([5, 8, 10, 2, 6, 1]));
